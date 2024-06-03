142d9754197d6332456bf8913a8f82514ef0078b
hbtch <gulnaz.baembitova@mail.ru>
2024-06-03 16:31:58 +0300

    changed the button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,3 @@
-import "./Button.css";
-
 const Button = (props) => {
     return (
         <button
--- src/screens/todoList/ToDoList.jsx
+++ src/screens/todoList/ToDoList.jsx
@@ -2,7 +2,7 @@ export function ToDoList() {
     return (
         <div>
             ToDoList Screen!
-        <button></button> 
+        <button className="todo-task">Новая задача</button> 
         </div>
     )
 }
