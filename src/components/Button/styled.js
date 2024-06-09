c35d0e570de9ac1c925cac7af03260bb796b7e61
hbtch <gulnaz.baembitova@mail.ru>
2024-06-09 16:22:53 +0300

    added styled.js

---
+++ src/components/Button/styled.js
@@ -0,0 +1,12 @@
+import Button from './Button'
+import styled from '@emotion/styled'
+
+export const StyledButton = styled(Button)`
+  background-color: ${(props) => (props.active ? '#ffb6c1' : '#ffc0cb')};
+  color: black;
+  margin: 10px;
+  border-radius: 15px;
+  &:hover {
+    background-color: ${(props) => (props.active ? '#ffa07a' : '#ff69b4')};
+  }
+`
--- src/screens/todoList/ToDoList.jsx
+++ src/screens/todoList/ToDoList.jsx
@@ -1,59 +1,19 @@
+import { StyledButton } from '../../components/Button/styled'
 import React, { useState } from 'react'
-import Button from '../../components/Button/Button'
-import styled from '@emotion/styled'
 
-const ButtonMake = styled(Button)`
-  background-color: ${(props) => (props.active ? '#ffb6c1' : '#ffc0cb')};
-  color: black;
-  margin: 10px;
-  border-radius: 15px;
-  &:hover {
-    background-color: ${(props) => (props.active ? '#ffa07a' : '#ff69b4')};
-  }
-`
-const ButtonInProgress = styled(Button)`
-  background-color: ${(props) => (props.active ? '#ffb6c1' : '#ffc0cb')};
-  color: black;
-  margin: 10px;
-  border-radius: 15px;
-  &:hover {
-    background-color: ${(props) => (props.active ? '#ffa07a' : '#ff69b4')};
-  }
-`
-const ButtonDone = styled(Button)`
-  background-color: ${(props) => (props.active ? '#ffb6c1' : '#ffc0cb')};
-  color: black;
-  margin: 10px;
-  border-radius: 15px;
-  &:hover {
-    background-color: ${(props) => (props.active ? '#ffa07a' : '#ff69b4')};
-  }
-`
 const ToDoList = () => {
-  const [active, setActive] = useState('1')
+  const [active, setActive] = useState(1)
   const handleClick = (id) => () => {
     setActive(id)
   }
   return (
     <div>
-      <br />
-      <ButtonMake
+      <StyledButton
         text="Сделать"
         active={active === '1'}
         onClick={handleClick('1')}
       />
-      <ButtonInProgress
-        text="В работе"
-        active={active === '2'}
-        onClick={handleClick('2')}
-      />
-      <ButtonDone
-        text="Готово"
-        active={active === '3'}
-        onClick={handleClick('3')}
-      />
     </div>
   )
 }
-
 export default ToDoList
