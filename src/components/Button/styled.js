b50466e4d8c8fff02f28e0f65ec5e5153a2c3457
hbtch <gulnaz.baembitova@mail.ru>
2024-06-09 16:30:13 +0300

    add styled.js

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -5,7 +5,7 @@ export const StyledButton = styled(Button)`
   background-color: ${(props) => (props.active ? '#ffb6c1' : '#ffc0cb')};
   color: black;
   margin: 10px;
-  border-radius: 15px;
+  border-radius: 18px;
   &:hover {
     background-color: ${(props) => (props.active ? '#ffa07a' : '#ff69b4')};
   }
